7cee354df935b668d33933a0b01fe0b1e526ce55
Venkatesh-529 <venkateshpatel529@gmail.com>
2026-01-21 16:57:34 +0530

    Day 104

---
+++ Day 104-Count Set Bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] countBits(int n) {
+        int res[]=new int[n+1];
+        res[0]=0;
+       for(int i=1;i<=n;i++){
+        if(i%2==0) res[i]=res[i/2];
+        else res[i]=res[i/2]+1;
+       }
+       return res;
+
+    }
+}
