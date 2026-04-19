459aaf0cc03293da7b4ad2a582819d1cf70649c9
Venkatesh-529 <venkateshpatel529@gmail.com>
2026-04-19 11:19:28 +0530

    Day 159

---
+++ Day 159- Maximum distance between pair of values.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDistance(int[] nums1, int[] nums2) {
+        int i = 0, j = 0;
+        int max = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+
+            if (nums1[i] <= nums2[j]) {
+                max = Math.max(max, j - i);
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return max;
+    }
+}
